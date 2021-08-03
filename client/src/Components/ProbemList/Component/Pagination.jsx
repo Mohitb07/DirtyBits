c91dd1ba6578d19982bfcd36daf3ead25571c015
mohitb07 <bmohit980@gmail.com>
2021-08-03 14:14:22 +0530

    pagination

---
+++ client/src/Components/ProbemList/Component/Pagination.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Pagination({ postPerPage, totalPosts, paginate}) {
+    const pageNumbers = []
+
+    for (let i = 1 ; i<= Math.ceil(totalPosts/postPerPage); i++) {
+        pageNumbers.push(i)
+    }
+    
+    return (  
+        <div style={{display:'flex', justifyContent:'center', marginTop:'2rem'}}>  
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className="page-item">
+                        <a onClick={() => paginate(number)} href="!#" className="page-link">
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+        </div>
+    )
+}
+
+export default Pagination
