3511f5263a2e6418614383246096a9c015894839
mohitb07 <bmohit980@gmail.com>
2021-08-04 16:31:02 +0530

    auth types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
 export const FETCH_USER = 'fetch_user';
 export const UPDATE_USER = 'update_user';
+export const LOGIN_SUCCESS = 'login_success'
+export const LOGIN_FAILED = 'login_failed'
+export const REGISTER_SUCESSS = 'register_success'
+export const REGISTER_FAILED = 'register_failed'
+export const LOGOUT = 'logout'
